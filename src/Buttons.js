0f57e47257c22219c012164ded08bf5d18961912
Christian Ambukita <christianambukita@gmail.com>
2022-01-07 14:00:19 +0100

    Format code

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,10 +1,13 @@
-export default function({handleClick}){
-
-    return(
-        <div className="buttons">
-            <p className="symbol" onClick={()=>handleClick(true)}>+</p>
-            <div className="vertical-line"></div>
-            <p className="symbol" onClick={()=>handleClick(false)}>-</p>
-        </div>
-    )
+export default function ({ handleClick }) {
+	return (
+		<div className='buttons'>
+			<p className='symbol' onClick={() => handleClick(true)}>
+				+
+			</p>
+			<div className='vertical-line'></div>
+			<p className='symbol' onClick={() => handleClick(false)}>
+				-
+			</p>
+		</div>
+	);
 }
